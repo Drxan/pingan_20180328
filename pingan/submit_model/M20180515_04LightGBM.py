049a0b4e7f91bd4cc079703f069da4312fc9f6cd
Drxan <yuwei8905@126.com>
2018-05-15 15:13:05 +0800

    16_lgb_0515

--- pingan/submit_model/M20180515_04LightGBM.py
+++ pingan/submit_model/M20180515_04LightGBM.py
@@ -18,10 +18,10 @@ import gc
 from sklearn.model_selection import train_test_split
 import copy
 from sklearn import metrics
-
+from math import radians, cos, sin, asin, sqrt
 
 # ---------submit------------
-'''
+
 path_train = '/data/dm/train.csv'
 path_test = '/data/dm/test.csv'
 path_test_out = "model/"  
@@ -30,7 +30,7 @@ path_test_out = "model/"
 path_train = r'D:\yuwei\study\competition\pingan/train_more.csv'  # 训练文件
 path_test = r'D:\yuwei\study\competition\pingan/test.csv'  # 测试文件
 path_test_out = "model/"
-
+'''
 
 train_dtypes = {'TERMINALNO': 'int32',
                 'TIME': 'int32',
@@ -71,6 +71,22 @@ def compute_index_statistics():
     pass
 
 
+def haversine1(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 （十进制度数）
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # 将十进制度数转化为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine公式
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # 地球平均半径，单位为公里
+    return c * r * 1000
+
+
 def extract_user_features(term):
     term = term.copy()
     term = term.drop_duplicates()
@@ -79,12 +95,23 @@ def extract_user_features(term):
     # [1] 行程量统计量
     # 总的行程记录数量
     record_num = term.shape[0]
-    features.append(record_num)
+    num_of_trips = term['TRIP_ID'].nunique()
+
+    features.extend([record_num, num_of_trips])
 
     term['month'] = term['TIME'].apply(lambda t: datetime.datetime.fromtimestamp(t).month)
     term['day'] = term['TIME'].apply(lambda t: datetime.datetime.fromtimestamp(t).day)
     term['weekday'] = term['TIME'].apply(lambda t: datetime.datetime.fromtimestamp(t).weekday())
     term['hour'] = term['TIME'].apply(lambda t: datetime.datetime.fromtimestamp(t).hour)
+
+    term['period'] = 0
+    term.loc[(term['hour'] >= 1) & (term['hour'] <= 3), 'period'] = 1
+    term.loc[(term['hour'] >= 11) & (term['hour'] <= 13), 'period'] = 2
+    term.loc[(term['hour'] >= 17) & (term['hour'] <= 19), 'period'] = 3
+
+    # 行程量最多、最少的时段,各时段记录量均值、方差、最大、最小值
+    features.extend(compute_num_statistics(term['period']))
+
     # 行程量最多、最少的月份,各月记录量均值、方差、最大、最小值
     features.extend(compute_num_statistics(term['month']))
 
@@ -133,6 +160,36 @@ def extract_user_features(term):
     features.extend(speed_std_hours)
     del speed_std_hour_sta
 
+    speed_period_group = term['SPEED'].groupby(term['period'])
+    # 各时段速度均值
+    speed_mean_period_sta = speed_period_group.mean()
+    speed_mean_periods = np.zeros(4, dtype=np.float32)
+    speed_mean_periods[speed_mean_period_sta.index] = speed_mean_period_sta
+    features.extend(speed_mean_periods)
+    del speed_mean_period_sta
+
+    # 各时段速度标准差
+    speed_std_period_sta = speed_period_group.std()
+    speed_std_periods = np.zeros(4, dtype=np.float32)
+    speed_std_periods[speed_std_period_sta.index] = speed_std_period_sta
+    features.extend(speed_std_periods)
+    del speed_std_period_sta
+
+    speed_callstate_group = term['SPEED'].groupby(term['CALLSTATE'])
+    # 各时段速度均值
+    speed_mean_callstate_sta = speed_callstate_group.mean()
+    speed_mean_callstates = np.zeros(5, dtype=np.float32)
+    speed_mean_callstates[speed_mean_callstate_sta.index] = speed_mean_callstate_sta
+    features.extend(speed_mean_callstates)
+    del speed_mean_callstate_sta
+
+    # 各时段速度标准差
+    speed_std_callstate_sta = speed_callstate_group.std()
+    speed_std_callstates = np.zeros(5, dtype=np.float32)
+    speed_std_callstates[speed_std_callstate_sta.index] = speed_std_callstate_sta
+    features.extend(speed_std_callstates)
+    del speed_std_callstate_sta
+
     # [3] 方向特征
     unknow_direc = (term['DIRECTION'] < 0).sum() / record_num
     features.append(unknow_direc)
@@ -175,25 +232,37 @@ def extract_user_features(term):
     time_dur = (term['TIME'].max() - term['TIME'].min()) / 3600.0+1.0
     lon_ratio = (max_lon - min_lon) / time_dur
     lat_ratio = (max_lat - min_lat) / time_dur
-    features.extend([max_lon, min_lon, max_lat, min_lat, lon_ratio, lat_ratio])
+    term = term.sort_values(by='TIME')
+    startlong = term.iloc[0]['LONGITUDE']
+    startlat = term.iloc[0]['LATITUDE']
+    dis_start = haversine1(startlong, startlat, 113.9177317, 22.54334333)  # 距离某一点的距离
+    features.extend([max_lon, min_lon, max_lat, min_lat, lon_ratio, lat_ratio, dis_start])
 
     return features
 
-feature_names=['record_num','busy_month', 'free_month', 'month_mean_num', 'month_std_num', 'month_max_num', 'month_min_num',
-              'busy_day', 'free_day', 'day_mean_num', 'day_std_num', 'day_max_num', 'day_min_num','busy_weekday', 'free_weekday',
-               'weekday_mean_num', 'weekday_std_num', 'weekday_max_num', 'weekday_min_num','busy_hour', 'free_hour', 'hour_mean_num',
-               'hour_std_num', 'hour_max_num', 'hour_min_num','weekday0_num','weekday1_num','weekday2_num','weekday3_num','weekday4_num',
-               'weekday5_num','weekday6_num','hour0_num', 'hour1_num', 'hour2_num', 'hour3_num', 'hour4_num', 'hour5_num', 'hour6_num',
-               'hour7_num', 'hour8_num', 'hour9_num', 'hour10_num', 'hour11_num', 'hour12_num', 'hour13_num', 'hour14_num', 'hour15_num',
-               'hour16_num', 'hour17_num', 'hour18_num', 'hour19_num', 'hour20_num', 'hour21_num', 'hour22_num', 'hour23_num','speed_mean',
-              'speed_max','speed_std','speed_median','hour0_speed_mean', 'hour1_speed_mean', 'hour2_speed_mean', 'hour3_speed_mean',
-               'hour4_speed_mean', 'hour5_speed_mean', 'hour6_speed_mean', 'hour7_speed_mean', 'hour8_speed_mean', 'hour9_speed_mean',
-               'hour10_speed_mean', 'hour11_speed_mean', 'hour12_speed_mean', 'hour13_speed_mean', 'hour14_speed_mean', 'hour15_speed_mean',
-               'hour16_speed_mean', 'hour17_speed_mean', 'hour18_speed_mean', 'hour19_speed_mean', 'hour20_speed_mean', 'hour21_speed_mean',
-               'hour22_speed_mean', 'hour23_speed_mean','hour0_speed_std', 'hour1_speed_std', 'hour2_speed_std', 'hour3_speed_std', 'hour4_speed_std',
+
+feature_names = ['record_num','num_of_trips','busy_period', 'free_period', 'period_mean_num', 'period_std_num',
+                 'period_max_num', 'period_min_num','busy_month', 'free_month', 'month_mean_num', 'month_std_num',
+                 'month_max_num', 'month_min_num','busy_day', 'free_day', 'day_mean_num', 'day_std_num', 'day_max_num',
+                 'day_min_num','busy_weekday', 'free_weekday','weekday_mean_num', 'weekday_std_num', 'weekday_max_num',
+                 'weekday_min_num','busy_hour', 'free_hour', 'hour_mean_num','hour_std_num', 'hour_max_num',
+                 'hour_min_num','weekday0_num','weekday1_num','weekday2_num','weekday3_num','weekday4_num',
+                 'weekday5_num','weekday6_num', 'hour0_num', 'hour1_num', 'hour2_num', 'hour3_num', 'hour4_num',
+                 'hour5_num', 'hour6_num','hour7_num', 'hour8_num', 'hour9_num', 'hour10_num', 'hour11_num',
+                 'hour12_num', 'hour13_num', 'hour14_num', 'hour15_num','hour16_num', 'hour17_num', 'hour18_num',
+                 'hour19_num', 'hour20_num', 'hour21_num', 'hour22_num', 'hour23_num','speed_mean','speed_max',
+                 'speed_std','speed_median','hour0_speed_mean', 'hour1_speed_mean', 'hour2_speed_mean',
+                 'hour3_speed_mean', 'hour4_speed_mean', 'hour5_speed_mean', 'hour6_speed_mean', 'hour7_speed_mean',
+                 'hour8_speed_mean', 'hour9_speed_mean','hour10_speed_mean', 'hour11_speed_mean', 'hour12_speed_mean',
+                 'hour13_speed_mean', 'hour14_speed_mean', 'hour15_speed_mean', 'hour16_speed_mean', 'hour17_speed_mean',
+                 'hour18_speed_mean', 'hour19_speed_mean', 'hour20_speed_mean', 'hour21_speed_mean','hour22_speed_mean',
+                 'hour23_speed_mean','hour0_speed_std', 'hour1_speed_std', 'hour2_speed_std', 'hour3_speed_std', 'hour4_speed_std',
                'hour5_speed_std', 'hour6_speed_std', 'hour7_speed_std', 'hour8_speed_std', 'hour9_speed_std', 'hour10_speed_std', 'hour11_speed_std',
                'hour12_speed_std', 'hour13_speed_std', 'hour14_speed_std', 'hour15_speed_std', 'hour16_speed_std', 'hour17_speed_std', 'hour18_speed_std',
-               'hour19_speed_std', 'hour20_speed_std', 'hour21_speed_std', 'hour22_speed_std', 'hour23_speed_std','unknow_direc','height_mean',
+               'hour19_speed_std', 'hour20_speed_std', 'hour21_speed_std', 'hour22_speed_std', 'hour23_speed_std','period0_speed_mean',
+                 'period1_speed_mean', 'period2_speed_mean','period3_speed_mean','period0_speed_std','period1_speed_std', 'period2_speed_std','period3_speed_std',
+                 'callstate0_speed_mean','callstate1_speed_mean', 'callstate2_speed_mean','callstate3_speed_mean','callstate4_speed_mean',
+                 'callstate0_speed_std','callstate1_speed_std', 'callstate2_speed_std','callstate3_speed_std','callstate4_speed_std','unknow_direc','height_mean',
                'height_max','height_min','height_std','height_median','hour0_height_mean', 'hour1_height_mean', 'hour2_height_mean', 'hour3_height_mean',
                'hour4_height_mean', 'hour5_height_mean', 'hour6_height_mean', 'hour7_height_mean', 'hour8_height_mean', 'hour9_height_mean',
                'hour10_height_mean', 'hour11_height_mean', 'hour12_height_mean', 'hour13_height_mean', 'hour14_height_mean', 'hour15_height_mean',
@@ -203,8 +272,9 @@ feature_names=['record_num','busy_month', 'free_month', 'month_mean_num', 'month
                'hour11_height_std', 'hour12_height_std', 'hour13_height_std', 'hour14_height_std', 'hour15_height_std', 'hour16_height_std',
                'hour17_height_std', 'hour18_height_std', 'hour19_height_std', 'hour20_height_std', 'hour21_height_std', 'hour22_height_std',
                'hour23_height_std','state0_ratio', 'state1_ratio', 'state2_ratio', 'state3_ratio', 'state4_ratio','max_lon','min_lon','max_lat',
-               'min_lat','lon_ratio','lat_ratio']
-
+               'min_lat','lon_ratio','lat_ratio','dis_start']
+cat_features = ['busy_period', 'free_period', 'busy_month', 'free_month', 'busy_day', 'free_day',
+                'busy_weekday', 'free_weekday','busy_hour', 'free_hour']
 lgb_params = {'boosting_type': 'gbdt',
               'colsample_bytree': 0.8,
               'learning_rate': 0.2,
@@ -221,10 +291,12 @@ lgb_params = {'boosting_type': 'gbdt',
               'reg_alpha': 0.005,
               'reg_lambda': 20,
               'subsample': 0.85,
-              'subsample_freq': 1}
+              'subsample_freq': 1,
+              'min_data_in_bin': 1,
+              'min_data': 1}
 
-param_dist = {'colsample_bytree': [0.5, 0.6, 0.7, 0.8, 0.9],
-              'max_bin': [50, 100, 168, 200, 255],
+param_dist_old = {'colsample_bytree': [0.1, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9],
+              'max_bin': [25, 50],  # [50, 100, 168, 200, 255]
               # 'max_depth': 5,
               'min_child_samples': [1, 6, 10, 20, 30, 50, 80],
               'min_child_weight': [0.01, 0.1, 1, 2],
@@ -235,17 +307,30 @@ param_dist = {'colsample_bytree': [0.5, 0.6, 0.7, 0.8, 0.9],
               'subsample': [0.7, 0.8, 0.9],
               'subsample_freq': [1, 3, 5, 8]}
 
+param_dist = {'colsample_bytree': list(np.arange(0.1, 1.0, 0.05)),
+              'max_bin': list(range(20, 100, 5)),
+              # 'max_depth': 5,
+              'min_child_samples': list(range(1, 200, 5)),
+              'min_child_weight': list(np.arange(0, 2, 0.01)),
+              'min_split_gain': list(np.arange(0, 2, 0.01)),
+              'num_leaves': list(range(3, 120, 2)),
+              'reg_alpha': list(np.arange(0, 20, 0.01)),
+              'reg_lambda': list(np.arange(0, 20, 0.01)),
+              'subsample': list(np.arange(0.5, 1.02, 0.02)),
+              'subsample_freq': list(range(1, 10, 1))}
 
-def randomized_search(x_train, x_val, y_train, y_val, estimator, modelparams, params_dist, feature_names, iter_search=60):
+
+def randomized_search(x_train, x_val, y_train, y_val, estimator, modelparams, params_dist, feature_names, cat_features, iter_search=60):
     best_model = None
     best_val_score = 999999
     best_params = modelparams
     for i in range(iter_search):
         temp_params = copy.deepcopy(modelparams)
         for key in params_dist.keys():
+            np.random.seed(int((time.time() % 100000)*1000))
             temp_params[key] = np.random.choice(params_dist[key])
         alg = estimator.__class__(**temp_params)
-        alg.fit(x_train, y_train, eval_metric='rmse', feature_name=feature_names, categorical_feature='auto', verbose=0)
+        alg.fit(x_train, y_train, eval_metric='rmse', feature_name=feature_names, categorical_feature=cat_features, verbose=0)
         preds = alg.predict(x_val)
         val_score = np.sqrt(metrics.mean_squared_error(y_val, preds))
         if val_score < best_val_score:
@@ -270,8 +355,9 @@ def process(CURRENT_PATH):
         targets.append(term['Y'].iloc[0])
     del train
     train_x = pd.DataFrame(train_x, columns=feature_names, dtype=np.float32)
+    train_x = train_x.fillna(-1)
     targets = np.array(targets, dtype=np.float32)
-    x_train, x_val, y_train, y_val = train_test_split(train_x, targets, test_size=0.25, random_state=9)
+    x_train, x_val, y_train, y_val = train_test_split(train_x, targets, test_size=0.3, random_state=9)
     gc.collect()
     end = time.time()
     print('time:{0}'.format((end-start)/60.0))
@@ -279,8 +365,8 @@ def process(CURRENT_PATH):
     print('[2]>> Finding the best parameters...')
     start = time.time()
     lgbr = lgb.LGBMRegressor(**lgb_params)
-    n_iter_search = 60
-    result_params, train_score, val_score = randomized_search(x_train, x_val, y_train, y_val, lgbr, lgb_params, param_dist, feature_names, n_iter_search)
+    n_iter_search = 66
+    result_params, train_score, val_score = randomized_search(x_train, x_val, y_train, y_val, lgbr, lgb_params, param_dist, feature_names, cat_features, n_iter_search)
     print('train_score:{0},val_score:{1}'.format(train_score, val_score))
     gc.collect()
     end = time.time()
@@ -292,7 +378,7 @@ def process(CURRENT_PATH):
     start = time.time()
     lgbr = lgb.LGBMRegressor(**result_params)
 
-    lgbr.fit(x_train, y_train, eval_metric='rmse', feature_name=feature_names, categorical_feature='auto',
+    lgbr.fit(x_train, y_train, eval_metric='rmse', feature_name=feature_names, categorical_feature=cat_features,
              eval_set=[(x_train, y_train), (x_val, y_val)], eval_names=['train', 'val'], verbose=0,
              early_stopping_rounds=100)
 
@@ -311,7 +397,7 @@ def process(CURRENT_PATH):
     print('[4]>> Fitting the final model...')
     print(result_params)
     lgbr = lgb.LGBMRegressor(**result_params)
-    lgbr.fit(train_x, targets, eval_metric='rmse', feature_name=feature_names, categorical_feature='auto', verbose=0)
+    lgbr.fit(train_x, targets, eval_metric='rmse', feature_name=feature_names, categorical_feature=cat_features, verbose=0)
 
     feat_imp = pd.Series(lgbr.feature_importances_, index=feature_names).sort_values(ascending=False)
     print(feat_imp.iloc[0:10])
@@ -330,6 +416,7 @@ def process(CURRENT_PATH):
         items.append(uid)
     del test
     test_x = pd.DataFrame(test_x, columns=feature_names, dtype=np.float32)
+    test_x = test_x.fillna(-1)
     gc.collect()
 
     end = time.time()
